a0e4c47511c4e4b451665ed0b97e02fe82c0f67e
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-13 08:14:50 +0300

    create a model with the sources class for creating instances of the class

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Sources:
+  """
+  Sources from different places 
+  """
+  def __init__(self, name, description, url, category, language, country):
+    self.name = name
+    self.description = description
+    self.url = url
+    self.category = category
+    self.language = language
+    self.country = country
