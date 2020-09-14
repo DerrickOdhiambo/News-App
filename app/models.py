e113b726bbbf9c6d970594c8b921a74ed04cb707
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-14 15:51:45 +0300

    create a class for top-headlines in the application

--- app/models.py
+++ app/models.py
@@ -2,10 +2,27 @@ class Sources:
   """
   Sources from different places 
   """
-  def __init__(self, name, description, url, category, language, country):
+  def __init__(self, id, name, description, url, category, language, country):
+    self.id = id
     self.name = name
     self.description = description
     self.url = url
     self.category = category
     self.language = language
     self.country = country
+
+
+class TopHeadlines:
+  """
+  Source articles from different sources
+  """
+
+  def __init__(self, author, title, description, url, urlToImage,publishedAt, content):
+    self.author = author
+    self.title = title
+    self.description = description
+    self.url = url
+    self.image_path = urlToImage
+    self.publishedAt = publishedAt
+    self.content = content
+
