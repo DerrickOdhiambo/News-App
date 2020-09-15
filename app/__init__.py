4072249c2a9a22d6c6817e23c92f7b629ae79dc6
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 10:59:05 +0300

    import the fontawesome from flask fontawesome and initialized it

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_fontawesome import FontAwesome
 from config import config_options
 
+fa = FontAwesome()
 
 def create_app(config_name):
 
   app = Flask(__name__)
+
+  #initializing fontawesome
+  fa.init_app(app)
 
   # creating the app configurations
   app.config.from_object(config_options[config_name])
