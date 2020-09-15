fea0f33cc165412e67e97c60d52339d00feb3dd7
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 14:16:46 +0300

    made minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,7 @@ from config import config_options
 
 def create_app(config_name):
 
-  app = Flask(__name__)
-
-  #initializing fontawesome
-  fa.init_app(app)
+  app =Flask(__name__)
 
   # creating the app configurations
   app.config.from_object(config_options[config_name])
