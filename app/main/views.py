9bf40b38ef6305bc2b518679e7fb33242a5233a7
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 14:12:59 +0300

    create routes for the business and sports html

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_new,get_source_details,get_everything
+from ..requests import get_new,get_source_details,get_everything,get_sports,get_business
 
 @main.route('/')
+@main.route('/home/')
 def index():
 
   sources = get_new()
@@ -21,3 +22,21 @@ def source(articles):
   title = 'NewsPoint Articles'
 
   return render_template('source.html',title=title, sources=sources,top_headlines = top_headlines)
+
+
+@main.route('/sports')
+def sports():
+
+  sports = get_sports()
+  title = 'NewsPoint Sports'
+
+  return render_template('sports.html', title = title, sports = sports)
+
+
+@main.route('/business')
+def business():
+
+  business = get_business()
+  title = 'NewsPoint Business'
+
+  return render_template('business.html', title = title, business = business)
