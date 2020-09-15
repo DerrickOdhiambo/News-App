566f3931aecb54bdaf514d873d88579898bcb6de
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 11:01:10 +0300

    add a class for the everything category

--- app/models.py
+++ app/models.py
@@ -2,14 +2,10 @@ class Sources:
   """
   Sources from different places 
   """
-  def __init__(self, id, name, description, url, category, language, country):
+  def __init__(self, id, name, description):
     self.id = id
     self.name = name
     self.description = description
-    self.url = url
-    self.category = category
-    self.language = language
-    self.country = country
 
 
 class TopHeadlines:
@@ -17,10 +13,23 @@ class TopHeadlines:
   Source articles from different sources
   """
 
-  def __init__(self, author, title, description, url, urlToImage,publishedAt, content):
+  def __init__(self, author, title, url, urlToImage,publishedAt, content):
     self.author = author
     self.title = title
-    self.description = description
+    self.url = url
+    self.image_path = urlToImage
+    self.publishedAt = publishedAt
+    self.content = content
+
+
+class Everything:
+  """
+  Source articles from different sources that shows different kinds of articles
+  """
+
+  def __init__(self, author, title, url, urlToImage, publishedAt, content):
+    self.author = author
+    self.title = title
     self.url = url
     self.image_path = urlToImage
     self.publishedAt = publishedAt
