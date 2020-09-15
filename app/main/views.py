2dbc34882d2afcb49828420cccfbff19440c1e13
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 10:57:19 +0300

    add titles adn also the get everything function to display articles in the main file

--- app/main/views.py
+++ app/main/views.py
@@ -1,20 +1,23 @@
 from flask import render_template
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_new,get_source_details
+from ..requests import get_new,get_source_details,get_everything
 
 @main.route('/')
 def index():
 
   sources = get_new()
-  return render_template('index.html', sources = sources)
+  everything = get_everything()
+  title = 'NewsPoint'
+  return render_template('index.html',title = title, sources = sources, everything = everything)
 
 
-@main.route('/source/<source>')
-def source(source):
+@main.route('/source/<articles>')
+def source(articles):
 
   sources = get_new()
-  top_headlines = get_source_details(source)
-  title = f'{source.name}'
+  top_headlines = get_source_details(articles)
+  print(top_headlines)
+  title = 'NewsPoint Articles'
 
-  return render_template('source.html', title=title, sources=sources,top_headlines = top_headlines)
+  return render_template('source.html',title=title, sources=sources,top_headlines = top_headlines)
