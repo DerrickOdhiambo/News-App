73268978e647c1e30324fb8b84daad06cfdae14a
DerrickOdhiambo <odhiamboderrick56@gmail.com>
2020-09-15 11:03:20 +0300

    add the headlines and everything base urls

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-  SOURCE_BASE_URL='https://newsapi.org/v2/sources?apiKey={}'
-  TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?apiKey={}'
+  SOURCE_BASE_URL='https://newsapi.org/v2/sources?language=en&apiKey={}'
+  TOP_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apikey={}'
+  EVERYTHING_URL = 'https://newsapi.org/v2/everything?q=trending&language=en&apiKey={}'
   NEW_API_KEY = os.environ.get('NEW_API_KEY')
 
 class ProdConfig(Config):
